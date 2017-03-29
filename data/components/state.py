5f39c36087e0d5433b413e5bf25a6cf150b6a0f7
Darri Valgarðsson <DarrMac@clients-pool-1.nat.ru.is>
2017-03-29 13:23:42 +0000

    Same as last commit, forgot to save a file

--- data/components/state.py
+++ data/components/state.py
@@ -1,11 +1,11 @@
 class State(object):
-	def __init__(self, marioPosX, marioPosY, enemies_group, obstacles):
+	def __init__(self, marioPosX, marioPosY, enemies_group, pipes):
 		self.xStart = marioPosX
 		self.yStart = marioPosY
 		self.xEnd = xStart + 200
 		self.yEnd = yStart + 200
 		self.enemies_group = enemies_group
-		self.obstacles = obstacles
+		self.pipes = pipes
 
 
 	def legalMoves(self, enemies_group):
@@ -13,8 +13,20 @@ class State(object):
 
 	def successorState(self, move):
 		if move == "right":
-			if(self.wayIsClear())
-				return State(self.xStart+6, self.yStart)
+			distance = self.nearestPipe()
+			if(distance > 6)
+				return State(self.xStart+6, self.yStart, self.enemies_group,self.pipes)
+			else:
+				return State(self.xStart+distance,self.yStart,self.enemies_group,self.pipes)
+		else:
+			
+
+	def nearestPipe(self):
+		for pip in self.pipes:
+			if pip.rect.x > self.xStart:
+				return pip.rect.x - self.xStart
+		return Infinity
+
 
 	def containsHole(self):
 		for groundBit in self.level.ground_list:
